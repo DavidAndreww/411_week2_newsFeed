ad8dc7e989c9daafa36fc8b094b76a1b70317bec
David Bille <d_bille@yahoo.com>
2020-01-14 20:39:53 -0600

    trying to get fetch to work, but CORS....

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
 import React from 'react';
 
 
-function App() {
-  return (
-    <h1>newsfeed</h1>
-  );
+class App extends React.Component {
+  state = {
+    articles: []
+  }
+
+  // fetchData = () => {
+  //   fetch('http://hn.algolia.com/api/v1/items/:id', {
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       'Accept': 'application/json'
+  //     }
+  //   })
+  //   .then(response => response.json())
+  //   .then(json => console.log(json))
+  //   .catch(err => console.log('Error!: ', err))
+  // }
+
+  fetchData = () => {
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const api=`${proxy}https://hn.algolia.com/api/`;
+    fetch(api, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log('Error encountered!: ', err))
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  
+
+  render () {
+    if (this.state.articles.length === 0){
+      return <p>Loading</p>
+    }
+    return (
+      <div>News Feed</div>
+    )
+  }
 }
 
 export default App;

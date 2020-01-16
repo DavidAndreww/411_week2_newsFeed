125f561f52b5d2a2e600e0404e537930ac5c48c1
David Bille <d_bille@yahoo.com>
2020-01-16 09:33:30 -0600

    changes to href target, open link in new window

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,9 +3,9 @@ import React from "react";
 const Article = ({postData}) => {
   return (
     <div className='article'>
-      <h3><a href={postData.url}>{postData.title}</a></h3>
+      <h3><a href={postData.url} target="blank">{postData.title}</a></h3>
       <br />
-      <p>{postData.author} | <span className="sub-text">Likes:{postData.points}</span></p>
+      <p><span className="sub-text">Posted By: {postData.author} || Likes:{postData.points}</span></p>
     </div>
   );
 };

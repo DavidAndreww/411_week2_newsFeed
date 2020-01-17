78dcca916aedbf26fc6ff8dd66d435f4588f44f8
David Bille <d_bille@yahoo.com>
2020-01-17 11:09:55 -0600

    my brain

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Article from "./Article";
 import NavBar from "./NavBar";
+import { comparePopular, compareDate, compareAuthor } from '../HelperFunctions'
 
 class App extends React.Component {
   state = {
@@ -35,8 +36,8 @@ class App extends React.Component {
   };
 
   // event.preventDefault is breaking code....searchfield works if query is performed when state changes using onChange, but not on form submit
-  searchTitles(event) {
-    console.log(event.target.value)
+  searchTitles(e) {
+    e.preventDefault()
     let { searchTerm } = this.state;
     console.log("submitted:", this.state.searchTerm);
     this.fetchData(searchTerm);
@@ -46,44 +47,14 @@ class App extends React.Component {
     if (this.state.articles.length === 0) {
       return <p className="loading">Loading Feed...</p>;
     }
-    let compareDate = (a, b) => {
-      let createdA = a.created_at;
-      let createdB = b.created_at;
 
-      let comparison = 0;
-      createdA > createdB ? (comparison = -1) : (comparison = 1);
-      return comparison;
-    };
-
-    let compareAuthor = (a, b) => {
-      let createdA = a.author.toLowerCase();
-      let createdB = b.author.toLowerCase();
-
-      let comparison = 0;
-      createdA > createdB ? (comparison = 1) : (comparison = -1);
-      return comparison;
-    };
-
-    let comparePopular = (a, b) => {
-      let createdA = a.points;
-      let createdB = b.points;
-
-      let comparison = 0;
-      createdA > createdB ? (comparison = -1) : (comparison = 1);
-      return comparison;
-    };
-    let { searchTerm, sortBy, articles } = this.state;
-
+    let { sortBy, articles } = this.state;
     if (sortBy === "date") {
       articles.sort(compareDate);
-      // https://www.sitepoint.com/sort-an-array-of-objects-in-javascript/
-      console.log("sort by date", searchTerm);
     } else if (sortBy === "author") {
       articles.sort(compareAuthor);
-      console.log("sort by author", searchTerm);
     } else if (sortBy === "popular") {
       articles.sort(comparePopular);
-      console.log("sort by popular", searchTerm);
     }
     return (
       <div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,21 +4,24 @@ const NavBar = ({ handleFormChange, searchTerm, searchTitles }) => {
   return (
     <nav>
       <h1>News Feed</h1>
-      <form onSubmit={() => searchTitles()}>
+      <form onSubmit={searchTitles}>
         <label>Sort by: </label>
-        <select id='sortBy' onChange={handleFormChange}>
-          <option value="date">
-            Most Recent
-          </option>
+        <select id="sortBy" onChange={handleFormChange}>
+          <option value="date">Most Recent</option>
           <option value="author">Author</option>
           <option value="popular">Most Popular</option>
         </select>
         <br />
         <label>Search Titles: </label>
-        <input id="searchTerm" value={searchTerm} type="text" onChange={handleFormChange}></input>
+        <input
+          id="searchTerm"
+          value={searchTerm}
+          type="text"
+          onChange={handleFormChange}
+        ></input>
         <button type="submit">Search</button>
       </form>
-    </nav> 
+    </nav>
   );
 };
 

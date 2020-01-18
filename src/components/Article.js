ae17c5704a84c7d84a96e82715dcd24b10318dc8
David Bille <d_bille@yahoo.com>
2020-01-18 14:09:18 -0600

    styling?

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,11 +2,10 @@ import React from "react";
 
 const Article = ({postData}) => {
   return (
-    <div className='article'>
+    <li className='article'>
       <h3><a href={postData.url} target="blank">{postData.title}</a></h3>
-      <br />
-      <p><span className="sub-text">Posted by: {postData.author} || Likes:{postData.points} || Date: {postData.created_at} ||</span></p>
-    </div>
+      <p><span className="sub-text">Posted by: {postData.author} || Likes:{postData.points} || Date: {postData.created_at}</span></p>
+    </li>
   );
 };
 
